1fba0c79910f8427be07b0ca485396415c285b44
Marcos Mascaraque <marcosmk2004@gmail.com>
2024-02-20 14:07:04 +0100

    Faker v2

--- videogames/src/main/java/com/example/videogames/VideogamesApplication.java
+++ videogames/src/main/java/com/example/videogames/VideogamesApplication.java
@@ -1,7 +1,10 @@
 package com.example.videogames;
 
+import com.example.videogames.services.InitialDataCreationService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VideogamesApplication {
@@ -11,4 +14,12 @@ public class VideogamesApplication {
 		SpringApplication.run(VideogamesApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner init(InitialDataCreationService service) {
+		return args -> {
+			service.createDefaultAdminUser();
+			service.createFakeCategories(10);
+			service.createFakeProducts(100);
+		};
+	}
 }
